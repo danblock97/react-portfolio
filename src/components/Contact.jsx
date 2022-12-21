d829abccb548a24b2d0a65c76301ed572a3e1f98
Dan <generalgoldiez@gmail.com>
2022-12-21 11:05:19 +0000

    Rounded form content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-[#aaccff] text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or send me an email at <strong>danblock1997@hotmail.co.uk</strong></p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" id="" rows="10" placeholder='Message'></textarea>
+            <input className='bg-[#ccd6f6] p-2 rounded-lg' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6] rounded-lg' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2 rounded-lg' name="message" id="" rows="10" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-[#aaccff] hover:border-[#aaccff] rounded-lg px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
