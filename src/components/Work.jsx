5c7e75526c7907cc4fd3a61107778b0b56b033ed
Dan <generalgoldiez@gmail.com>
2022-12-21 19:58:39 +0000

    Add new weather app to work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -23,12 +23,12 @@ const Work = () => {
                 React JS Weather App
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://dbweatherapp.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/danblock97/react-weather-app">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
